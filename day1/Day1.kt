b9357507926cec65873ab187a567244d25a41a1c
SofieHoefkens <sofie.hoefkens@gmail.com>
2023-12-01 13:13:04 +0100

    clean day1 part1

--- day1/Day1.kt
+++ day1/Day1.kt
@@ -4,9 +4,12 @@ import java.io.File
 fun readFileLines(fileName:String): List<String>
     = File(fileName).useLines { it.toList() }
 
+fun part1(): Int {
+    return readFileLines("day1/input_day1.txt")
+            .map { line -> line.filter { it.isDigit() } }
+            .sumOf { line -> (line.first().toString() + line.last()).toInt() }
+}
+
 fun main() {
-    val numbers = readFileLines("day1/input_day1.txt")
-            .map{line -> line.filter {it.isDigit()}}
-            .map{line -> (line.first().toString() + line.last()).toInt()}
-    println(numbers.sum())
+    println(part1())
 }
